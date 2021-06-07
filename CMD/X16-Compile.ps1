bd566ac73b1f5531568de3adebdcce2f50dbbb13
FlightControl <sven.van.de.velde@telenet.be>
2021-06-07 22:34:17 +0200

    updates

--- CMD/X16-Compile.ps1
+++ CMD/X16-Compile.ps1
@@ -2,4 +2,4 @@ echo $args[0] $args[1]
 $filedir=$args[0]
 $filename=$args[1]
 #$filedir="C:\Users\svenv\OneDrive\Documents\GitHub\X16_Code"
-kickc_dev -t=cx16 -a -Sc -Si -Ocoalesce -Xassembler=-symbolfile "${filedir}/${filename}"
+kickc_dev -t=cx16 -a -Sc -Si -Xassembler=-symbolfile "${filedir}/${filename}"
