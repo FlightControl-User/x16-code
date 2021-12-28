06ae13214c0fce23389f64b4d9e2ad425572dc8a
Flight_Control <sven.van.de.velde@telenet.be>
2021-12-28 21:43:11 +0100

    Updates

--- CMD/X16-cl65-Compile.ps1
+++ CMD/X16-cl65-Compile.ps1
@@ -1,9 +1,5 @@
-echo $args[0] $args[1]
+Write-Output  $args[0] $args[1]
 $filedir=$args[0]
 $filename=$args[1]
-#$filedir="C:\Users\svenv\OneDrive\Documents\GitHub\X16_Code"
 
-kickc_dev -t=cx16 -a -Sc -Si -Onouplift -vasmout -Xassembler=-symbolfile "${filedir}/${filename}" 
-# kickc_dev -t=cx16 -fragment pbuz1_derefidx_vbuc1_eq_vbuc2_then_la1
-# kickc_dev -t=cx16 -v -vfragment -a -Sc -Si -Onouplift -Xassembler=-symbolfile "d:/Users/svenv/OneDrive/Documents/GitHub/X16_Code/cx16-equinoxe/equinoxe-flightengine.c"
-# kickc_dev -t=cx16 -a -Sc -Si -Onouplift -Xassembler=-symbolfile "d:/Users/svenv/OneDrive/Documents/GitHub/X16_Code/cx16-equinoxe/equinoxe-flightengine.c"
+cl65 -t cx16 -O "$filedir/$filename" 
