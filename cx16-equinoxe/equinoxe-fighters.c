fc8ffec7ecb46d9d9355ac124f4dcbe20af53bbc
Flight_Control <sven.van.de.velde@telenet.be>
2022-01-03 17:01:22 +0100

    Progress in staging

--- cx16-equinoxe/equinoxe-fighters.c
+++ cx16-equinoxe/equinoxe-fighters.c
@@ -9,22 +9,34 @@ void DrawFighters()
     heap_handle fighter_handle = stage.fighter_list;
 
     do {
-
-        gotoxy(0,10);
-        printf("draw                   ...");
-
         // gotoxy(0,11); printf("debug = %u, handle = %x, next = %x", debug_count++, fighter_handle, ((Entity*)heap_data_ptr(fighter_handle))->next);
         Entity* fighter = (Entity*)heap_data_ptr(fighter_handle);
-        sprite_enable(fighter->sprite_offset, fighter->sprite_type);
-        sprite_animate(fighter->sprite_offset, fighter->sprite_type, fighter->state_animation);
-        sprite_position(fighter->sprite_offset, fighter->x, fighter->y);
-
         heap_handle engine_handle = fighter->engine_handle;
-        if(engine_handle) {
-            Entity* engine = (Entity*)heap_data_ptr(engine_handle);
-            sprite_enable(engine->sprite_offset, engine->sprite_type);
-            sprite_animate(engine->sprite_offset, engine->sprite_type, engine->state_animation);
-            sprite_position(engine->sprite_offset, engine->x, engine->y);
+        unsigned char disable = 0;
+        if(fighter->x > -64) {
+            if(fighter->x < 640) {
+                sprite_enable(fighter->sprite_offset, fighter->sprite_type);
+                sprite_animate(fighter->sprite_offset, fighter->sprite_type, fighter->state_animation);
+                sprite_position(fighter->sprite_offset, fighter->x, fighter->y);
+                if(engine_handle) {
+                    Entity* engine = (Entity*)heap_data_ptr(engine_handle);
+                    sprite_enable(engine->sprite_offset, engine->sprite_type);
+                    sprite_animate(engine->sprite_offset, engine->sprite_type, engine->state_animation);
+                    sprite_position(engine->sprite_offset, engine->x, engine->y);
+                }
+            } else {
+                disable = 1;
+            }
+        } else {
+            disable = 1;
+        }
+
+        if(disable) {
+            sprite_disable(fighter->sprite_offset);
+            if(engine_handle) {
+                Entity* engine = (Entity*)heap_data_ptr(engine_handle);
+                sprite_disable(engine->sprite_offset);
+            }
         }
 
         fighter_handle = ((Entity*)heap_data_ptr(fighter_handle))->next;
