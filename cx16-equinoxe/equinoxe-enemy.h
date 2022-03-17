251ca73b3ea5a0c756f185ef00247bb53dd81e51
Flight_Control <sven.van.de.velde@telenet.be>
2022-03-17 17:02:16 +0100

    Working version

--- cx16-equinoxe/equinoxe-enemy.h
+++ cx16-equinoxe/equinoxe-enemy.h
@@ -1,12 +1,13 @@
 #ifndef equinoxe_enemy_h
 #define equinoxe_enemy_h
 
-#include <cx16-heap.h>
 #include "equinoxe.h"
 #include "equinoxe-flightengine.h"
 #include "equinoxe-collision.h"
 
 typedef struct entity_s Enemy;
+
+
 
 void AddEnemy(char t, signed int x, signed int y);
 
